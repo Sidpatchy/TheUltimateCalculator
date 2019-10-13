717b9f57dfcdd3a254e50da433c96b30e2c0695a
Rainverm38 <35241490+Rainverm38@users.noreply.github.com>
2019-10-13 12:51:08 -0500

    Initial commit
    Wrote a readme.
    Calculator is currently able to solve the discriminant of a quadratic and solve for X when the answer is positive. Currently, it is unable to calculate imaginary answers.

---
+++ README.md
@@ -0,0 +1,4 @@
+# RomeBot by Rainverm38
+
+This is a very basic calculator, whose main purpose is to check answers on my math homework.
+Why make my own when I could just use an online calculator? This is, by far, more fun.
---
+++ TheUltimateCalculator.py
@@ -0,0 +1,71 @@
+# The Ultimate Calculator by Rainverm38
+# You can find more info at: https://github.com/Rainverm38/TheUltimateCalculator
+
+import math
+
+menu = 1
+while menu == 1:
+    print('Welcome to the ultimate calculator! To get started, please select an option from the following list:')
+    print('0) Exit')
+    print('1) Simplifing the discriminant of a quadratic')
+    print('2) Solve for the \'x\' value of a quadratic')
+    print()
+    operation = float(input('Please insert an operation from the list: '))
+    
+    if operation == 1:
+        a = 1
+        print('Selected: 1) simplifing the discriminant of a quadratic')
+        while a == 1:
+            a = int(input('Insert \'a\' value: '))
+            b = int(input('Insert \'b\' value: '))
+            c = int(input('Insert \'c\' value: '))
+            print()
+            ans = (b**2) - (4*a*c)
+
+            if ans > 0:
+                print('There Are 2 Real Solutions, The Answer is: ', ans)
+            elif ans == 0:
+               print('There Is 1 Real Solution, The Answer is: ', ans)
+            elif ans < 0:
+                print('There Are 2 Imaginary Solutions, The Answer is: ', ans)
+    
+            print('The equation is: ', b, '^2 - 4(', a, ')', '(', c, ') = ', ans)
+            print()
+            exit = input('Press enter to continue calculating discriminants or type \'x\' to return to the menu: ')
+            if exit == 'x':
+                a = 0
+                print()
+            else:
+                a = 1
+    elif operation == 2:
+        b = 1
+        print('Selected: 2) Solve for the \'x\' value of a quadratic')
+        while b == 1:
+            a = int(input('Insert \'a\' value: '))
+            b = int(input('Insert \'b\' value: '))
+            c = int(input('Insert \'c\' value: '))
+            print()
+
+            d = (b**2) - (4*a*c)
+            
+            if d > 0:
+                bPlus = (-b + math.sqrt(d)) / (2 * a)
+                bMinus = (-b - math.sqrt(d)) / (2 * a)
+                print('There Are 2 Real Solutions, The X Values Are: ', bPlus, bMinus)
+            elif d == 0:
+               bPlus = (-b + math.sqrt(d)) / (2 * a)
+               print('There Is 1 Real Solution, The Answer is: ', bPlus)
+            elif d < 0:
+                print('There Are 2 Imaginary Solutions, I Can\'t Calculate Those Yet.')
+            else:
+                print('If you are somehow seeing this, I am very, very broken.')
+            
+            exit = input('Press enter to continue calculating discriminants or type \'x\' to return to the menu: ')
+            if exit == 'x':
+                b = 0
+                print()
+            else:
+                b = 1
+            
+    elif operation == 0:
+        exit()
